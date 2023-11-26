2ebe1e4d1539def9f2e23e191f246ce5394800fd
Phoenics-space <136085461+Phoenics-space@users.noreply.github.com>
2023-11-26 12:46:11 +0900

    stratos-template commit

--- src/main/java/stratos/mod/ExampleModClient.java
+++ src/main/java/stratos/mod/ExampleModClient.java
@@ -1,11 +1,15 @@
 package stratos.mod;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
+import stratos.block.ModBlocks;
 
 public class ExampleModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
 
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.AIR_MAHOE_LICHEN, RenderLayer.getTranslucent());
     }
 }
 
