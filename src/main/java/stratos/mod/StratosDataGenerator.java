462185fd46c20dd6e706a4e5bcb7256e6f3595a7
Phoenics-space <136085461+Phoenics-space@users.noreply.github.com>
2023-12-01 21:24:06 +0900

    stratos-template commit

--- src/main/java/stratos/mod/StratosDataGenerator.java
+++ src/main/java/stratos/mod/StratosDataGenerator.java
@@ -2,7 +2,11 @@ package stratos.mod;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
 import stratos.datagen.*;
+import stratos.world.ModDimensions;
+
 
 public class StratosDataGenerator implements DataGeneratorEntrypoint {
 	@Override
@@ -14,6 +18,12 @@ public class StratosDataGenerator implements DataGeneratorEntrypoint {
 		pack.addProvider(ModLootTableProvider::new);
 		pack.addProvider(ModModelProvider::new);
 		pack.addProvider(ModRecipeProvider::new);
+
+		@Override
+		public void buildRegistry(RegistryBuilder registryBuilder) {
+
+			registryBuilder.addRegistry(RegistryKeys.DIMENSION_TYPE, ModDimensions::bootstrapType);
+		}
 
 	}
 }
